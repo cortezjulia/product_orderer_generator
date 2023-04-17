6cf36024f02625e6adffe294fb80a01c6cde682a
Julia Cortez <jcgaucha@gmail.com>
2023-04-17 09:09:55 -0300

    initialized packages

---
+++ FILES/__init__.py
@@ -0,0 +1,1 @@
+from FILES.products_file import products
--- orderer_generator.py
+++ orderer_generator.py
@@ -10,14 +10,7 @@
 
 import copy 
 
-products= [
-    {'nome': 'Produto 5', 'preco': 10.00},
-    {'nome': 'Produto 1', 'preco': 22.32},
-    {'nome': 'Produto 3', 'preco': 10.11},
-    {'nome': 'Produto 2', 'preco': 105.87},
-    {'nome': 'Produto 4', 'preco': 69.90},
-]
-
+from FILES import products
 
 #step 1 - 10% increment
 
